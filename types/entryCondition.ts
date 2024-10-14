7f4e797312712e1433d64cae5366fe6a3c3c2be4
Luke <lfer0019@student.monash.edu>
2024-10-14 21:51:39 +1100

    Changed user to participant

--- types/entryCondition.ts
+++ types/entryCondition.ts
@@ -1,41 +1,41 @@
-import { User } from "./user";
+import { Participant } from "./participant";
 
 export type EntryCondition = {
   condition: string;
-  check: (user: User) => boolean;
+  check: (Participant: Participant) => boolean;
 };
 
 export const Follow: EntryCondition = {
   condition: "follow",
   check: () => {
-    //TODO: use X api to check if user is following the account
-    return true; // check if user is following the account
+    //TODO: use X api to check if Participant is following the account
+    return true; // check if Participant is following the account
   },
 };
 
 export const Like: EntryCondition = {
   condition: "like",
   check: () => {
-    //TODO: use X api to check if user is liking the post
-    return true; // check if user is liking the post
+    //TODO: use X api to check if Participant is liking the post
+    return true; // check if Participant is liking the post
   },
 };
 
 export const Comment: EntryCondition = {
   condition: "comment",
   check: () => {
-    // TODO: use X api to check if user is commenting on the post
-    return true; // check if user is commenting on the post
+    // TODO: use X api to check if Participant is commenting on the post
+    return true; // check if Participant is commenting on the post
   },
 };
 
 
 /**
- * Checks if a user meets all the specified entry conditions.
+ * Checks if a Participant meets all the specified entry conditions.
  *
- * @param user - The user to be checked against the conditions.
- * @param conditions - An array of entry conditions that the user must meet.
- * @returns A boolean indicating whether the user meets all the conditions.
+ * @param Participant - The Participant to be checked against the conditions.
+ * @param conditions - An array of entry conditions that the Participant must meet.
+ * @returns A boolean indicating whether the Participant meets all the conditions.
  */
-export const checkConditions = (user: User, conditions: EntryCondition[]) =>
-  conditions.every((condition) => condition.check(user));
+export const checkConditions = (Participant: Participant, conditions: EntryCondition[]) =>
+  conditions.every((condition) => condition.check(Participant));
--- types/raffle.ts
+++ types/raffle.ts
@@ -1,11 +1,12 @@
-import { User } from "./user";
+import { Participant } from "./participant";
 import { EntryCondition, checkConditions } from "./entryCondition";
 
 export type Raffle = {
   id: string;
   name: string;
-  participants: User[];
-  winner?: User;
+  entryConditions: EntryCondition[];
+  participants: Participant[];
+  winner?: Participant;
 };
 
 /**
@@ -14,5 +15,30 @@ export type Raffle = {
  * @param conditions - An array of entry conditions that the participants must meet.
  * @returns - An array of participants who meet all the conditions.
  */
-export const getValidParticipants = (participants: User[], conditions: EntryCondition[]) =>
-  participants.filter((participant) => checkConditions(participant, conditions));
+export const getValidParticipants = (
+  participants: Participant[],
+  conditions: EntryCondition[]
+) =>
+  participants.filter((participant) =>
+    checkConditions(participant, conditions)
+  );
+
+/**
+ * Selects a random winner from the list of participants who meet the entry conditions.
+ * @param raffle - The Raffle to draw a winner for.
+ * @param participants - The array of participants who entered the raffle.
+ * @returns - The winner of the raffle.
+ */
+export const drawWinner = (raffle: Raffle, participants: Participant[]) => {
+  const validParticipants = getValidParticipants(
+    participants,
+    raffle.entryConditions
+  );
+  // Check if there are any valid participants
+  if (validParticipants.length === 0) {
+    throw new Error("No participants meet the entry conditions.");
+  }
+  const winner =
+    validParticipants[Math.floor(Math.random() * validParticipants.length)];
+  return winner;
+};
