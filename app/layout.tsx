c76020ee02effee50f3761c2c741a67ad81d16d4
Luke <lfer0019@student.monash.edu>
2024-10-17 12:39:34 +1100

    added navbar that redirects to homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from 'next';
 import '@fontsource/inter';
 import './globals.css';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import { IconButton, Toolbar } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Raffle Manager',
@@ -14,7 +19,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {renderAppBar()}
+        {children}
+      </body>
     </html>
   );
 }
+
+const renderAppBar = () => (
+  <AppBar
+    position="static"
+    sx={{
+      bgcolor: '#1E1E1E',
+      color: '#D9D9D9',
+    }}
+  >
+    <Toolbar>
+      <Link href="/">
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <HomeIcon />
+        </IconButton>
+      </Link>
+    </Toolbar>
+  </AppBar>
+);
